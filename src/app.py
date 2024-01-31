d10a81219d9a07bed41894fefcce443a04a198a4
Matthew Chin <mattchin813@gmail.com>
2024-01-31 14:45:13 -0800

    added text box and button

--- src/app.py
+++ src/app.py
@@ -4,19 +4,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
+     <p>Hello there! Enter a name or a sentence of your choosing. 
+     Soon this will be a website for NBA players' metrics for stats gurus, fantasy players, or curiosity. </p>
          <input name="user_input">
          <input type="submit" value="Submit!">
+    <p>Who is your favorite NBA player?</p>     
+        <input name="player_name">
+        <input type="submit" value="Add">
      </form>
      '''
+
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
-
-
-
+    player_name = request.form.get("player_name", "")
+    return "You entered: " + input_text + " and your favorite NBA player is: " + player_name
