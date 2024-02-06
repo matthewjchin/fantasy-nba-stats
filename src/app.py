9e61ccb6decc948a1ed6df3b1814fc00cf9310a4
Matthew Chin <mattchin813@gmail.com>
2024-02-06 12:41:00 -0800

    added another submit button

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def main():
     <form action="/echo_user_input" method="POST">
         <p>Hello there! Enter a name or a sentence of your choosing. 
         Soon this will be a website for NBA basketball players' metrics for stats gurus, 
-        fantasy players, or curiosity. 
+        fantasy players, or curiosity on a player's performance. 
         
         <br>
         
@@ -21,6 +21,7 @@ def main():
         </p>
         
         <input name="user_input">
+        <input type="submit" value="Submit!">
         
     </form>
     
