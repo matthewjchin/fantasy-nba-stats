49708cfe677d53059c43730965f41db09c224d01
Matthew Chin <mattchin813@gmail.com>
2024-02-07 20:20:12 -0800

    refactoring

--- src/app.py
+++ src/app.py
@@ -3,24 +3,15 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.sqlite3'
 #
 # db = SQLAlchemy(app)
 # class Player(db.Model):
 
+from flask import Flask, request
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-
-    return "Greeting: " + input_text
-
-
-@app.route("/echo_nba", methods=["POST"])
-def echo_nba_player():
-    player_name = request.form.get("player_name", "")
-    return "<p>Your favorite NBA player is: " + player_name + "</p>"
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -28,26 +19,36 @@ def main():
     return '''
     <h1>Enter a Greeting</h1>
     <form action="/echo_user_input" method="POST">
-        <p>Enter a name or a sentence of your choosing.
-        Soon this will be a website for NBA basketball players' metrics for stats gurus,
-        fantasy players, or curiosity on a player's performance.
+        <p>Hello there! Enter a name or a sentence of your choosing. 
+        Soon this will be a website for NBA basketball players' metrics for stats gurus, 
+        fantasy players, or curiosity. 
 
         <br>
 
-        Suitable for fantasy drafts and trades, sports betting, statisticians, and gurus.
-        In the meantime, enter a greeting to get started.
+        Suitable for fantasy drafts and trades, sports betting, statisticians, and gurus. 
+        In the meantime, enter a greeting to get started. 
         </p>
 
         <input name="user_input">
         <input type="submit" value="Submit!">
-
     </form>
 
     <form action="/echo_nba" method="POST">
-        <p>Who is your favorite NBA player?</p>
-        <p>Preferably, enter a current player's name with first and last name.</p>  
+        <p>Who is your favorite NBA player?</p>     
         <input name="player_name">
         <input type="submit" value="Submit!">
     </form>
     '''
+
+
+@app.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "Greeting: " + input_text
+
+
+@app.route("/echo_nba", methods=["POST"])
+def echo_nba_player():
+    player_name = request.form.get("player_name", "")
+    return "<p>Your favorite NBA player is: " + player_name + "</p>"
 
