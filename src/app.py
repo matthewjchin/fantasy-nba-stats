435d69161acc21fd1b3c70daa7ba61ac66c9ef27
Matthew Chin <mattchin813@gmail.com>
2024-02-09 21:57:11 -0800

    Changes to SQLite DB

--- src/app.py
+++ src/app.py
@@ -26,16 +26,16 @@ def get_player_name_active():
 
     user_input = request.form["user_input"]
 
-    # custom_headers = {
-    #     'Host': 'stats.nba.com',
-    #     'Connection': 'keep-alive',
-    #     'Cache-Control': 'max-age=0',
-    #     'Upgrade-Insecure-Requests': '1',
-    #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-    #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-    #     'Accept-Encoding': 'gzip, deflate, br',
-    #     'Accept-Language': 'en-US,en;q=0.9',
-    # }
+    custom_headers = {
+        'Host': 'stats.nba.com',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+    }
 
     player_info = dict()
     player_stats = list()
